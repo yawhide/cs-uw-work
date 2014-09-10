7c6d267bef266e451a6106a7955306f8d1e452a2
ubuntu <Stefan Wojcik>
2014-09-10 10:17:18 -0700

    finished a0

--- kern/startup/menu.c
+++ kern/startup/menu.c
@@ -396,6 +396,22 @@ cmd_kheapstats(int nargs, char **args)
 	return 0;
 }
 
+/*
+ * Command to set dbflags true.
+ *
+ *
+ */
+static
+int
+cmd_dbflags(int nargs, char **args)
+{
+	(void)nargs;
+	(void)args;
+
+	dbflags = 0x0010;
+	return 0;
+}
+
 ////////////////////////////////////////
 //
 // Menus.
@@ -437,6 +453,7 @@ static const char *opsmenu[] = {
 	"[sync]    Sync filesystems          ",
 	"[panic]   Intentional panic         ",
 	"[q]       Quit and shut down        ",
+	"[dth]     enables DB_THREADS flag   ",
 	NULL
 };
 
@@ -548,6 +565,8 @@ static struct {
 	{ "q",		cmd_quit },
 	{ "exit",	cmd_quit },
 	{ "halt",	cmd_quit },
+	/* a0 operations */
+	{ "dth",	cmd_dbflags},
 
 #if OPT_SYNCHPROBS
 	/* in-kernel synchronization problem(s) */
